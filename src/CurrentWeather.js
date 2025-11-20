6ccd1b8adf592c0cdd776cf330d4b98b767e57b8
aayusht200 <aayush.t200@gmail.com>
2025-11-20 13:39:20 +0530

    api class for current weather

---
+++ src/CurrentWeather.js
@@ -0,0 +1,8 @@
+export class CurrentWeather {
+    async get() {
+        const currentWeather = await fetch(
+            'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/mumbai?unitGroup=us&include=current&key=DN7B5PPTW2GKZRF7E8T6Y3UFF&contentType=json'
+        );
+        return currentWeather.json();
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import "./style.css";
+import './style.css';
+import { CurrentWeather } from './CurrentWeather.js';
+
+const weather = new CurrentWeather();
+const data = await weather.get();
+
+console.log(data);
