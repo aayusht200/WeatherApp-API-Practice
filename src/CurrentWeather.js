8e27f91f31d248b41e63780fd69c82faf3a4832d
aayusht200 <aayush.t200@gmail.com>
2025-11-21 18:39:28 +0530

    added current location funcationality

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,21 +1,22 @@
+import { getCurrentCity } from './getLocation.js';
+
 export class CurrentWeather {
     async get() {
-        // 1. Check cache
+        let city = (await getCurrentCity()).replaceAll(' ', '').toLowerCase();
+        if (city === '') {
+            city = 'mumbai';
+        }
+
         const cached = localStorage.getItem('weatherData');
         if (cached) {
             return JSON.parse(cached);
         }
-        let value = 'mumbai';
-        // 2. Make API call once
         const res = await fetch(
-            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${value}?unitGroup=us&include=current&key=DN7B5PPTW2GKZRF7E8T6Y3UFF&contentType=json`
+            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&include=current&key=DN7B5PPTW2GKZRF7E8T6Y3UFF&contentType=json`
         );
         let data = await res.json();
-        console.log(data);
         data = { location: data.address, currentConditions: data.currentConditions, today: data.days[0] };
-        // 3. Save to cache
         localStorage.setItem('weatherData', JSON.stringify(data));
-
         return data;
     }
 }
---
+++ src/getLocation.js
@@ -0,0 +1,23 @@
+export async function getCurrentCity() {
+    const locationData = await getCity();
+    return locationData.city;
+}
+
+async function getCity() {
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+    };
+    const pos = await new Promise((success, err) => {
+        navigator.geolocation.getCurrentPosition(success, err, options);
+    });
+    const locationData = await getLocation(pos.coords.latitude, pos.coords.longitude);
+    return { city: locationData.results[0].components.city };
+}
+async function getLocation(latitude, longitude) {
+    const res = await fetch(
+        `https://api.opencagedata.com/geocode/v1/json?q=${latitude}%2C+${longitude}&key=cfe0703b6e6d4c46911fea3990efaa6c&pretty=1`
+    );
+    return await res.json();
+}
